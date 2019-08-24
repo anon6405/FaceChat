8e23c77edf91a34ae897b1640bf7931957d7118b
Shubham Saini <shubham6405@gmail.com>
2019-08-24 10:50:22 +0530

    added live typing & 80%(approx) accuracy

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-	res.render('index')
+	res.render('index', {qs: req.query})
 })
 
 server = app.listen(3000)
